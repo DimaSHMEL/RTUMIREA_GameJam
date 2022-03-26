a1c6cd6dc38438f2f073b1b1847e3d28fcd8a935
DimaSHMEL <55811573+DimaSHMEL@users.noreply.github.com>
2022-03-26 05:08:06 +0300

    JumpKing jumpSystem and more
    Because of sock structure of the gypotalamus I thought about comming up to exit
    Some i made jumpking like system. There would be location Blue and Red/ and in it will be located bounuses to collect and some hidden doors.
    1. JumpSystem like in jump king.
    2. Control over player

--- RTUMIREA_GameJam/Assets/Player/Scripts/PlayerScript.cs
+++ RTUMIREA_GameJam/Assets/Player/Scripts/PlayerScript.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerScript : MonoBehaviour
 {
+    //Movement values
+    public float speedMovement;
+    public float jumpForce;
     private Rigidbody2D rb;
-
+    private bool facingRight = true;
+    //Checkers values
+    public LayerMask whatIsGround, whatIsWall;
+    public BoxCollider2D groundCheck, wallCheck;
+    private bool onGround = false;
+    //Control values
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
+    //Score values
+    private int collectables = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +33,60 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 speedXY = playerInputActions.Player.Move.ReadValue<Vector2>();
+        if(Math.Abs(speedXY.x) > 0.15f && onGround)
+            rb.velocity = new Vector2(speedXY.x *  speedMovement, rb.velocity.y);
+        else if(Math.Abs(speedXY.x) == 0.0f && rb.velocity.y == 0.0f )
+            rb.velocity = new Vector2(0, rb.velocity.y);
+
         
-        rb.velocity = new Vector2(speedXY.x, rb.velocity.y);
+    }
+    IEnumerator holdJump()
+    {
+        float jumpPower = 0;
+        float numberOfSeconds = 0;
+        while (playerInputActions.Player.Jump.IsPressed())
+        {
+            if (jumpPower < 1.2f)
+            {
+                jumpPower += 0.1f;
+                numberOfSeconds += 0.1f;
+            }
+            
+            yield return new WaitForSeconds(0.1f);
+        }
+        Debug.Log(jumpPower * jumpForce);
+        Debug.Log(numberOfSeconds);
+        if(facingRight)
+            rb.velocity = new Vector2(speedMovement, jumpPower * jumpForce);
+        else
+            rb.velocity = new Vector2(-speedMovement, jumpPower * jumpForce);
+
     }
     private void Update()
     {
-        if (playerInputActions.Player.Jump.triggered)
+        if (playerInputActions.Player.Jump.triggered && onGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10);
-            Debug.Log("jump");
+            StartCoroutine(holdJump());
         }
+        checkStatuses();
+
+    }
+    private void checkStatuses()
+    {
+        Vector2 speedXY = playerInputActions.Player.Move.ReadValue<Vector2>();
+        onGround = groundCheck.IsTouchingLayers(whatIsGround);
+        if (speedXY.x < 0f && facingRight)
+            flip();
+        else if (speedXY.x > 0f && !facingRight)
+            flip();
+        if (wallCheck.IsTouchingLayers(whatIsWall))
+            rb.AddForce(new Vector2(0, -9.8f));
+    }
+    private void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
