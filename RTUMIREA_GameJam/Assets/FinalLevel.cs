6ca75c44bb920e40019c04890ecb99df8568d7e5
DimaSHMEL <55811573+DimaSHMEL@users.noreply.github.com>
2022-03-27 21:17:57 +0300

    Final ready

--- RTUMIREA_GameJam/Assets/FinalLevel.cs
+++ RTUMIREA_GameJam/Assets/FinalLevel.cs
@@ -11,9 +11,25 @@ public class FinalLevel : MonoBehaviour
             StartCoroutine(StartCountDown());
         }
     }
+    public AudioSource ma;
     IEnumerator StartCountDown()
     {
+        yield return new WaitForSeconds(4);
+        GameObject cam = GameObject.Find("Camera");
+        Destroy(cam.GetComponent<CameraScript>());
+        Destroy(GameObject.Find("UI"));
+        cam.transform.position = new Vector3(-11.6f, -1.7f, -14f);
         yield return new WaitForSeconds(1);
+        while (cam.GetComponent<Camera>().orthographicSize < 32)
+        {
+            cam.GetComponent<Camera>().orthographicSize += 0.25f;
+            yield return new WaitForSeconds(0.025f);
+        }
+        yield return new WaitForSeconds(1f);
+        ma.Play();
+        yield return new WaitForSeconds(1f);
+        cam.transform.position = new Vector3(-11.6f, -1.7f, -16f);
+        yield return new WaitForSeconds(2);
         GameObject.Find("Savings").GetComponent<SaveLoadSys>().ResetData();
         SceneManager.LoadScene("Menu");
     }
